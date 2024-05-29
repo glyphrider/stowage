7ed57a88236e68ccef1f730cd6b47bb3c4e32cce
Brian H. Ward <glyphrider@gmail.com>
2024-05-29 15:56:32 -0400

    more hand crafted neovim

---
+++ dot-config/nvim/init.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " "
+
+require("custom.plugin.lazy_bootstrap")
+require("custom.plugin")
+require("custom.keymap")
---
+++ dot-config/nvim/lua/custom/plugin/init.lua
@@ -0,0 +1,7 @@
+require("lazy").setup({
+	    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+-- or                              , branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    }
+})
