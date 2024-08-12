af0c210300ffadeb4a3aae0fc505dc66535ed013
Brian H. Ward <glyphrider@gmail.com>
2024-08-12 15:47:52 -0400

    updates to nvim

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -209,25 +209,23 @@ require("lazy").setup({
 
 	{ -- Useful plugin to show you pending keybinds.
 		"folke/which-key.nvim",
-		event = "VimEnter", -- Sets the loading event to 'VimEnter'
-		config = function() -- This is the function that runs, AFTER loading
-			require("which-key").setup()
-
-			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-				["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-				["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-			})
-			-- visual mode
-			require("which-key").register({
-				["<leader>h"] = { "Git [H]unk" },
-			}, { mode = "v" })
-		end,
+		event = "VeryLazy", -- Sets the loading event to 'VimEnter'
+    keys = {
+      { "<leader>c", desc = "[C]ode", },
+      { "<leader>c_", hidden = true, },
+      { "<leader>d", desc = "[D]ocument", },
+      { "<leader>d_", hidden = true, },
+      { "<leader>r", desc = "[R]ename", },
+      { "<leader>r_", hidden = true, },
+      { "<leader>s", desc = "[S]earch", },
+      { "<leader>s_", hidden = true, },
+      { "<leader>w", desc = "[W]orkspace", },
+      { "<leader>w_", hidden = true, },
+      { "<leader>t", desc = "[T]oggle", },
+      { "<leader>t_", hidden = true, },
+      { "<leader>h", desc = "[H]unk", mode = "v", },
+      { "<leader>h_", hidden = true, },
+    },
 	},
 
 	-- NOTE: Plugins can specify dependencies.
